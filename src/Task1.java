89430ccfd1dcc34ed3877b5b38acee08007e6d7c
Anastasia Boruk <anastasia.borukk@gmail.com>
2025-09-12 16:05:42 +0300

    first commit. ProjectBoruk

---
+++ src/Task1.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String developerLastName = "Борук";
+        System.out.println(developerLastName);
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        System.out.print(currentDateTime);
+    }
+}
