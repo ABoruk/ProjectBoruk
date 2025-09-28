6de0ef9ffe9c204149a88de6389d00cec7ae3511
Anastasia Boruk <anastasia.borukk@gmail.com>
2025-09-28 16:17:39 +0300

    Task5

---
+++ src/homework/Task6/Task1.java
@@ -0,0 +1,29 @@
+package homework.Task6;
+
+public class Task1 {
+    public static void main(String[] args) {
+//        int [] array=new int[5];
+//        array [0]=5;
+//        array [1]=3;
+//        array [2]=4;
+//        array [3]=1;
+//        System.out.println(array[3]);
+        int [] array1={1,2,3,4,5,6,7,8,9,10};
+
+        for (int k:array1)
+        {
+            if (k%2!=0)
+            {
+           System.out.println(0);
+        }
+         else
+            {
+                System.out.println(k);
+            }
+
+        }
+
+
+
+    }
+}
