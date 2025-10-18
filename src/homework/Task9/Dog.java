e817e04e13c53247aef8d7dc2d2a247c275182d8
Anastasia Boruk <anastasia.borukk@gmail.com>
2025-10-18 11:32:15 +0300

    Task 9,10

---
+++ src/homework/Task9/Dog.java
@@ -0,0 +1,77 @@
+package homework.Task9;
+
+//В классе собака определить имя, порода, средний вес.
+//В классе Собака реализовать методы: лаять, кусать, бегать, играть, прыгать.
+// При этом, в консоли должны выводится имя собаки вместе с описанием действия (пример. Рекс прыгает).
+public class Dog extends Animal {
+    private String name;
+    private String breed;
+    private double avg_weight;
+
+    public Dog(String color, int age, String food, String breed, String name, double avg_weight) {
+        super(color, age, food);
+        this.breed = breed;
+        this.name = name;
+        this.avg_weight = avg_weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public double getAvg_weight() {
+        return avg_weight;
+    }
+
+    public void sound() {
+        System.out.println(name + " лает");
+    }
+
+    public void kusat() {
+        System.out.println(name + " кусает");
+
+    }
+
+    public void run() {
+        System.out.println(name + " бегает");
+    }
+
+    public void jump() {
+        System.out.println(name + " прыгает");
+    }
+
+    // Переопределить методы, которые есть в родительском классе животное (издание звуков, животное играет)  в классах Собака и Птица.
+    public void play() {
+        System.out.println(name + " играет");
+    }
+
+    public String toString() {
+        String result = super.toString();
+        result = result + " Имя: " + name + " Порода: " + breed + " Средний вес " + avg_weight;
+        return result;
+    }
+
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Dog)) {
+            return false;
+        }
+        Dog dog1 = (Dog) obj;
+
+        if (name.equals(dog1.getName()) && breed.equals(dog1.getBreed()) && avg_weight == getAvg_weight()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int hashCode() {
+        int result = (int) (name.hashCode() + breed.hashCode() + avg_weight * 2);
+        return result;
+    }
+
+
+}
