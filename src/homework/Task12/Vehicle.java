8a7484a36b8e9ed401c13d2bfd956d64556b7152
Anastasia Boruk <anastasia.borukk@gmail.com>
2025-10-26 18:22:07 +0300

    Task12

---
+++ src/homework/Task12/Vehicle.java
@@ -0,0 +1,27 @@
+package homework.Task12;
+
+public class Vehicle {
+    String name;
+    int amount;
+
+    public Vehicle(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
